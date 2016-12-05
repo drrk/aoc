08b8c258503d490c4d4baf8a668540328f550c03
Kimball Johnson <kj@chef.io>
2016-12-05 00:38:56 +0000

    Day 4

---
+++ day4-1.rb
@@ -0,0 +1,25 @@
+require 'pp'
+sum = 0
+File.open('day4_input').each do |line|
+  parts = line.chomp.strip.split('-')
+  freq = {}
+  sector, check = /(\d+)\[(\w+)\]/.match(parts.pop).captures
+  parts.each do |part|
+    part.each_char do |c|
+      if freq.key?(c)
+        freq[c] += 1
+      else
+        freq[c] = 1
+      end
+    end
+  end
+  sorted_freq = freq.sort_by { |letter,count| [ -count, letter ] }
+  valid = true
+  i = 0
+  check.each_char do | c |
+    valid = false if c != sorted_freq[i][0]
+    i += 1
+  end
+  sum += sector.to_i if valid
+end
+print "Valid Sector Sum #{sum}\n"
---
+++ day4-2.rb
@@ -0,0 +1,38 @@
+require 'pp'
+
+def caesar_cipher(string, shift = 1)
+  alphabet   = Array('a'..'z')
+  encrypter  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  string.chars.map { |c| encrypter.fetch(c, " ") }
+end
+
+sum = 0
+File.open('day4_input').each do |line|
+  parts = line.chomp.strip.split('-')
+  freq = {}
+  sector, check = /(\d+)\[(\w+)\]/.match(parts.pop).captures
+  parts.each do |part|
+    part.each_char do |c|
+      if freq.key?(c)
+        freq[c] += 1
+      else
+        freq[c] = 1
+      end
+    end
+  end
+  sorted_freq = freq.sort_by { |letter,count| [ -count, letter ] }
+  valid = true
+  i = 0
+  check.each_char do | c |
+    valid = false if c != sorted_freq[i][0]
+    i += 1
+  end
+  if valid 
+    parts.each do |part|
+      print caesar_cipher(part,sector.to_i).join
+      print '-'
+    end
+    print "#{sector}\n"
+  end
+end
+print "Valid Sector Sum #{sum}\n"
